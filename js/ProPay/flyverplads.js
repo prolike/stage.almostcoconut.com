e9c607fc7d950e4093813d984920264e01628730
Circle CI by @Prolike <circleci@prolike.io>
2020-01-03 13:06:09 +0000

    Built from GitHub repo prolike/almostcoconut.com version 5bda965

---
+++ js/ProPay/flyverplads.js
@@ -0,0 +1,29 @@
+function submitform() {
+  var data = $("#signup_form").serializeArray();
+  var customer = Object.assign({}, ...data.map(item => ({ [item.name]: item.value, "generate_handle": true })));
+
+  var config = {
+    "plan": "plan-3852a",
+    "signup_method": "link"
+  }
+
+  var finalData = {
+    "plan": config.plan,
+    "signup_method": config.signup_method,
+    "generate_handle": true,
+    "create_customer": customer
+  }
+
+  axios({
+    method: 'post',
+    url: 'https://europe-west1-propay-dev.cloudfunctions.net/api',
+    data: finalData,
+    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+  })
+    .then(() => {
+      window.location.href = "/success/"
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
---
+++ js/ProPay/roomb.js
@@ -0,0 +1,30 @@
+function submitform() {
+    var data = $("#signup_form").serializeArray();
+    var customer = Object.assign({}, ...data.map(item => ({ [item.name]: item.value, "generate_handle": true })));
+
+    var config = {
+    //change plan to the correct one
+      "plan": "plan-91ca4",
+      "signup_method": "link"
+    }
+  
+    var finalData = {
+      "plan": config.plan,
+      "signup_method": config.signup_method,
+      "generate_handle": true,
+      "create_customer": customer
+    }
+  
+    axios({
+      method: 'post',
+      url: 'https://europe-west1-propay-dev.cloudfunctions.net/api',
+      data: finalData,
+      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+    })
+      .then(() => {
+        window.location.href = "/success/"
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
